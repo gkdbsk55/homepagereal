d60a1e1a5d02fec8289ba15e59e2186cb6c6cf49
gkdbsk55 <gkdbsk55@gmail.com>
2022-08-21 00:06:54 +0900

    first commit

---
+++ src/test/java/com/example/yuna/YunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
